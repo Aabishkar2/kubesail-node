1b71b31d767bbbffe0324f846cb0240acc935b83
Aabis <aabishkar2@gmail.com>
2020-05-20 23:21:50 +0545

    src updated

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ var app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get("/", function (req, res) {
-  res.send("hello aabishkar");
+  res.send("Hello to my first gitlab ci and kubernetes project! Yeah!");
 });
 
 app.listen(4000, function () {
