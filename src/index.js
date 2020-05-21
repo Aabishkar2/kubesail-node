6c328c84f647ab693feb8535440c1637e1691f3c
Aabis <aabishkar2@gmail.com>
2020-05-21 20:23:05 +0545

    src updated

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ var app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get("/", function (req, res) {
-  res.send("Hello sanjay dai");
+  res.send("Yeah! Finally the ci/cd is running on kubernetes");
 });
 
 app.listen(4000, function () {
